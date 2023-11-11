662cff271eb418b408fc3d35db686df9612c8513
anas garbouj <anaskping@gmail.com>
2023-11-11 15:00:35 +0100

    Revert "adding a full stack blog component"
    This reverts commit 87a86791006e4fd661a32eaf6b6a239c2aa2c830.

--- front/src/app/models/comment.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Comment {
-    _id?: string;
-    content: string;
-    name: string;
-    blog: string;
-    status: string; // Include status to check if it's approved
-    createdAt?: Date;
-  }
-  
--- front/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  trackAnalyticID: "G-XE9X54XCMH",
-  apiUrl: 'http://yourproductionapiurl.com/api'
-
-};
