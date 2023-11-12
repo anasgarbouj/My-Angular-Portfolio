0f0b8ffded8de126f1cbea1ffce37e4904db63a6
anas garbouj <anaskping@gmail.com>
2023-11-12 03:26:23 +0100

    changing apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   trackAnalyticID: "G-XE9X54XCMH",
-  apiUrl: 'http://yourproductionapiurl.com/api'
+  apiUrl: 'https://anasback.netlify.app/api'
 
 };
